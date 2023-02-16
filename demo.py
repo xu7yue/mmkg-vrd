bc0827d7b3cffd019b42ce0d07e33403e0b953d7
xu7yue <xu7yue@gmail.com>
2023-02-16 16:17:53 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,45 @@
+import os
+from typing import List, Dict
+
+from PIL import Image
+import gradio as gr
+from mmkg_vrd import VRD
+
+pretrained_ckpt ='custom_io/ckpt/RX152FPN_reldn_oi_best.pth'
+labelmap_file ='custom_io/ji_vrd_labelmap.json'
+freq_prior ='custom_io/vrd_frequency_prior_include_background.npy'
+
+def inference(input_image: str):
+    
+    vrd = VRD(pretrained_ckpt=pretrained_ckpt, labelmap_file=labelmap_file, freq_prior=freq_prior)
+    
+    dir_name = os.path.dirname(input_image)
+    base_name = os.path.basename(input_image)
+    ext = base_name.split('.')[-1]
+    base_name_woext = '.'.join(base_name.split('.')[:-1])
+    save_file = os.path.join(dir_name, f'{base_name_woext}.vrd.{ext}')
+    
+    vrd.det_and_vis(
+        img_file=input_image,
+        save_file=save_file
+    )
+
+    return save_file
+
+def main():
+    gr.Interface(
+        inference,
+        inputs=[
+            gr.Image(type="filepath"),
+        ],
+        outputs=gr.Image(type="filepath"),
+        examples=[
+            [
+                'custom_io/imgs/1024px-Gen_Robert_E_Lee_on_Traveler_at_Gettysburg_Pa.jpg', 
+            ]
+        ],
+      
+    ).launch(share=True)
+
+if __name__ == "__main__":
+    main()
